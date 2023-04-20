5f3c494144fc0bc37405dd92e235f48628ebb0b9
unknown <qlysander27@gmail.com>
2023-04-20 14:59:37 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+var Age = confirm("Are you 25 years old and up?")
+if(Age){
+    document.write("I am 25 years old and up. <br /> <br />")
+}
+else{
+    document.write("I am not 25 years old and up. <br /> <br />")
+}
+
+var Number = prompt("Enter a number less than 100")
+if(Number <= 100){
+    alert("You have entered a number less than 100.")
+}
+else{
+    alert("You have entered a number over 100.")
+}
+
+for(var i = 0; i <=30; i++){
+    console.log(i + "<br />")
+}
+
+for(var i = 0; i <=40; i++){
+    document.write(i + "<br /> <br />")
+}
+
+for(var i = 40; i >=10; i--){
+    document.write(i + "<br />")
+}
+
